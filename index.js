e4409358a87ba2aac3fae44f4fcfc68c5849ce76
Lloyd <stevens.lloyd@Gmail.com>
2023-04-03 19:09:20 +0000

    playing with parseint

---
+++ index.js
@@ -0,0 +1,10 @@
+let num1 = parseInt(process.argv[2]);
+let num2 = parseInt(process.argv[3]);
+let num3 = parseInt(process.argv[4]);
+
+let sum = num1 + num2 + num3;
+console.log(`The sum of ${num1} ${num2} ${num3} is ${sum}`)
+
+
+
+console.log(parseInt("11111111", 2))
